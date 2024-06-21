86da8cf6f82522f5ff716f05620f2ede42a747fb
tapskabs <141030379+tapskabs@users.noreply.github.com>
2024-06-21 18:26:18 +0200

    Menu's added

---
+++ Assets/Scripts/EndMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+public class EndMenu : MonoBehaviour
+{
+   
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void Instruction()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
